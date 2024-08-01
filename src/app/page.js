b4d423ccc3380071f87aafbe571512311a021858
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-01 21:58:16 +0200

    Changed background color

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import ScrollToTopButton from "./components/ScrollToTop";
 
 export default function Home() {
   return (
-    <main className="min-h-screen py-16 px-8 text-stone-950">
+    <main className="min-h-screen py-16 px-8 text-stone-950 bg-stone-50/50">
       <SearchBox />
       <ScrollToTopButton />
     </main>
